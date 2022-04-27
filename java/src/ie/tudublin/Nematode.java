5d7f8ebe94160cd8769694636eb721474b0b52d4
McFarrell118 <michaelfarrell342@hotmail.com>
2022-04-27 16:54:47 +0100

    constructors

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,64 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+import processing.data.TableRow;
+
+
+public class Nematode {
+
+    private String name;
+    private int length;
+    private int limbs;
+    private String gender;
+    private int eyes;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public int getLimbs() {
+        return limbs;
+    }
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getEyes() {
+        return eyes;
+    }
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
+                + name + "]";
+    }
+    
+    public Nematode(String name, int length, int limbs, String gender, int eyes) {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+
+    
+}
+
+
